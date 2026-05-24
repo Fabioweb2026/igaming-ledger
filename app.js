cd1ad2ad2d55209c4d89f3d83f85d8c04d9badb3
Fábio de Andrade Carvalho <facfabioandrade@gmail.com>
2026-05-24 01:33:30 -0300

    app.js
    Signed-off-by: Fábio de Andrade Carvalho <facfabioandrade@gmail.com>

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+let currentBalance = 0;
 
+async function loadPlayer() {
+
+    const response =
+        await fetch('../database/players.json');
+
+    const players =
+        await response.json();
+
+    const player = players[0];
+
+    currentBalance = player.walletBalance;
+
+    document.getElementById('player-name').innerText =
+        player.username;
+
+    updateBalance();
+
+    document.getElementById('bonus-balance').innerText =
+        '€' + player.bonusBalance;
+
+    document.getElementById('kyc-status').innerText =
+        player.kycStatus;
+}
+
+function updateBalance() {
+
+    document.getElementById('wallet-balance').innerText =
+        '€' + currentBalance.toFixed(2);
+}
+
+function depositMoney() {
+
+    currentBalance += 100;
+
+    updateBalance();
+
+    alert('Deposit Successful!');
+}
